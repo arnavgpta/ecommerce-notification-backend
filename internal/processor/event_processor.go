8331849539ac8eadf2e8c58c5d4ce4591d43c12b
Arnav gupta <garnav02@gmail.com>
2026-02-12 17:17:29 +0530

    feat: integrate rule engine into async worker

--- internal/processor/event_processor.go
+++ internal/processor/event_processor.go
@@ -1,18 +1,26 @@
 package processor
 
 import (
+	"context"
 	"log"
 
 	"github.com/arnavgpta/ecommerce-notification-backend/internal/models"
+	"github.com/arnavgpta/ecommerce-notification-backend/internal/repository"
+	"github.com/arnavgpta/ecommerce-notification-backend/internal/rules"
 )
 
 type EventProcessor struct {
-	queue chan models.CreateEventRequest
+	queue            chan models.CreateEventRequest
+	notificationRepo *repository.NotificationRepository
 }
 
-func NewEventProcessor(bufferSize int) *EventProcessor {
+func NewEventProcessor(
+	bufferSize int,
+	notificationRepo *repository.NotificationRepository,
+) *EventProcessor {
 	return &EventProcessor{
-		queue: make(chan models.CreateEventRequest, bufferSize),
+		queue:            make(chan models.CreateEventRequest, bufferSize),
+		notificationRepo: notificationRepo,
 	}
 }
 
@@ -29,9 +37,27 @@ func (p *EventProcessor) Enqueue(event models.CreateEventRequest) {
 }
 
 func (p *EventProcessor) handleEvent(event models.CreateEventRequest) {
-	log.Printf("Processing event: %s for user %d",
-		event.EventType,
+
+	notificationType, shouldNotify :=
+		rules.DetermineNotification(event)
+
+	if !shouldNotify {
+		return
+	}
+
+	err := p.notificationRepo.CreateNotification(
+		context.Background(),
 		event.UserID,
+		notificationType,
 	)
 
+	if err != nil {
+		log.Printf("failed to create notification: %v", err)
+		return
+	}
+
+	log.Printf("notification created: %s for user %d",
+		notificationType,
+		event.UserID,
+	)
 }
